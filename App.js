7037d709b42977fd18bcc3307e2eab911c51a646
joninfante <jonjoninfante@gmail.com>
2021-08-02 11:09:04 -0400

    React native components, add and delete items

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import React, {useState} from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, FlatList, Alert } from 'react-native';
 import Header from './components/Header';
-import { uuid } from 'uuidv4';
+import ListItem from './components/ListItem'
+import AddItem from './components/AddItem'
 
 const App = () => {
   const [items, setItems] = useState([
-    {id: uuid(), text: 'Milk'},
-    {id: uuid(), text: 'Eggs'},
-    {id: uuid(), text: 'Bread'},
-    {id: uuid(), text: 'Juice'},
-
+    {id: 1, text: 'Milk'},
+    {id: 1, text: 'Eggs'},
+    {id: 3, text: 'Bread'},
+    {id: 4, text: 'Juice'},
   ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  }
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', [{text: 'OK', onPress: () => console.log("OK Pressed")}], {cancelable: true});
+    } else {
+      setItems(prevItems => {
+        return [{id: Math.random(), text},...prevItems];
+    });
+  
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Header/>
+      <AddItem  addItem={addItem}/>
+      <FlatList 
+        data={items} 
+        renderItem={({item}) => (<ListItem item={item} deleteItem={deleteItem} />
+      )} />
     </View>
   );
 };
@@ -21,7 +44,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 60,
+    paddingTop: 0,
   },
 
 });
